1d47d6a59028f6c3c4ace3d18348ade149e6cc86
MichaelAndreyeshchev <michaelandrev@gmail.com>
2023-04-16 17:26:47 -0500

    Other code stuff

--- run_nerf_helper.py
+++ run_nerf_helper.py
@@ -5,6 +5,7 @@ import torch.nn as nn
 import numpy as np
 import imageio
 import json
+import torch.nn.functional as F
 from typing import Optional, Tuple
 
 
@@ -13,7 +14,7 @@ from typing import Optional, Tuple
 def img2mse(x, y): return torch.mean(torch.square(x - y))
 
 
-def mse2psnr(x): return -10.*torch.log(x)/torch.log(10.)
+def mse2psnr(x): return -10.*torch.log(x)/torch.log(torch.Tensor([10.]))
 
 
 def to8b(x): return (255*np.clip(x, 0, 1)).astype(np.uint8)
@@ -80,27 +81,22 @@ def get_embedder(multires, i=0):
 '''
 # Model architecture
 def init_nerf_model(D=8, W=256, input_ch=3, input_ch_views=3, output_ch=4, skips=[4], use_viewdirs=False):
-
     relu = tf.keras.layers.ReLU()
     def dense(W, act=relu): return tf.keras.layers.Dense(W, activation=act)
-
     print('MODEL', input_ch, input_ch_views, type(
         input_ch), type(input_ch_views), use_viewdirs)
     input_ch = int(input_ch)
     input_ch_views = int(input_ch_views)
-
     inputs = tf.keras.Input(shape=(input_ch + input_ch_views))
     inputs_pts, inputs_views = tf.split(inputs, [input_ch, input_ch_views], -1)
     inputs_pts.set_shape([None, input_ch])
     inputs_views.set_shape([None, input_ch_views])
-
     print(inputs.shape, inputs_pts.shape, inputs_views.shape)
     outputs = inputs_pts
     for i in range(D):
         outputs = dense(W)(outputs)
         if i in skips:
             outputs = tf.concat([inputs_pts, outputs], -1)
-
     if use_viewdirs:
         alpha_out = dense(1, act=None)(outputs)
         bottleneck = dense(256, act=None)(outputs)
@@ -115,177 +111,182 @@ def init_nerf_model(D=8, W=256, input_ch=3, input_ch_views=3, output_ch=4, skips
         outputs = tf.concat([outputs, alpha_out], -1)
     else:
         outputs = dense(output_ch, act=None)(outputs)
-
     model = tf.keras.Model(inputs=inputs, outputs=outputs)
     return model
-
 '''
 
 
 # Model architecture
 class NeRF(nn.Module):
-  r"""
-  Neural radiance fields module.
-  """
-  def __init__(
-    self,
-    d_input: int = 3,
-    n_layers: int = 8,
-    d_filter: int = 256,
-    skip: Tuple[int] = (4,),
-    d_viewdirs: Optional[int] = None
-  ):
-    super().__init__()
-    self.d_input = d_input
-    self.skip = skip
-    self.act = nn.functional.relu
-    self.d_viewdirs = d_viewdirs
+    def __init__(self, D=8, W=256, input_ch=3, input_ch_views=3, output_ch=4, skips=[4], use_viewdirs=False):
+        """ 
+        """
+        super(NeRF, self).__init__()
+        self.D = D
+        self.W = W
+        self.input_ch = input_ch
+        self.input_ch_views = input_ch_views
+        self.skips = skips
+        self.use_viewdirs = use_viewdirs
+        
+        self.pts_linears = nn.ModuleList(
+            [nn.Linear(input_ch, W)] + [nn.Linear(W, W) if i not in self.skips else nn.Linear(W + input_ch, W) for i in range(D-1)])
+        
+        ### Implementation according to the official code release (https://github.com/bmild/nerf/blob/master/run_nerf_helpers.py#L104-L105)
+        self.views_linears = nn.ModuleList([nn.Linear(input_ch_views + W, W//2)])
 
-    # Create model layers
-    self.layers = nn.ModuleList(
-      [nn.Linear(self.d_input, d_filter)] +
-      [nn.Linear(d_filter + self.d_input, d_filter) if i in skip \
-       else nn.Linear(d_filter, d_filter) for i in range(n_layers - 1)]
-    )
+        ### Implementation according to the paper
+        # self.views_linears = nn.ModuleList(
+        #     [nn.Linear(input_ch_views + W, W//2)] + [nn.Linear(W//2, W//2) for i in range(D//2)])
+        
+        if use_viewdirs:
+            self.feature_linear = nn.Linear(W, W)
+            self.alpha_linear = nn.Linear(W, 1)
+            self.rgb_linear = nn.Linear(W//2, 3)
+        else:
+            self.output_linear = nn.Linear(W, output_ch)
 
-    # Bottleneck layers
-    if self.d_viewdirs is not None:
-      # If using viewdirs, split alpha and RGB
-      self.alpha_out = nn.Linear(d_filter, 1)
-      self.rgb_filters = nn.Linear(d_filter, d_filter)
-      self.branch = nn.Linear(d_filter + self.d_viewdirs, d_filter // 2)
-      self.output = nn.Linear(d_filter // 2, 3)
-    else:
-      # If no viewdirs, use simpler output
-      self.output = nn.Linear(d_filter, 4)
-  
-  def forward(
-    self,
-    x: torch.Tensor,
-    viewdirs: Optional[torch.Tensor] = None
-  ) -> torch.Tensor:
-    r"""
-    Forward pass with optional view direction.
-    """
+    def forward(self, x):
+        input_pts, input_views = torch.split(x, [self.input_ch, self.input_ch_views], dim=-1)
+        h = input_pts
+        for i, l in enumerate(self.pts_linears):
+            h = self.pts_linears[i](h)
+            h = F.relu(h)
+            if i in self.skips:
+                h = torch.cat([input_pts, h], -1)
 
-    # Cannot use viewdirs if instantiated with d_viewdirs = None
-    if self.d_viewdirs is None and viewdirs is not None:
-      raise ValueError('Cannot input x_direction if d_viewdirs was not given.')
+        if self.use_viewdirs:
+            alpha = self.alpha_linear(h)
+            feature = self.feature_linear(h)
+            h = torch.cat([feature, input_views], -1)
+        
+            for i, l in enumerate(self.views_linears):
+                h = self.views_linears[i](h)
+                h = F.relu(h)
 
-    # Apply forward pass up to bottleneck
-    x_input = x
-    for i, layer in enumerate(self.layers):
-      x = self.act(layer(x))
-      if i in self.skip:
-        x = torch.cat([x, x_input], dim=-1)
+            rgb = self.rgb_linear(h)
+            outputs = torch.cat([rgb, alpha], -1)
+        else:
+            outputs = self.output_linear(h)
 
-    # Apply bottleneck
-    if self.d_viewdirs is not None:
-      # Split alpha from network output
-      alpha = self.alpha_out(x)
+        return outputs    
 
-      # Pass through bottleneck to get RGB
-      x = self.rgb_filters(x)
-      x = torch.concat([x, viewdirs], dim=-1)
-      x = self.act(self.branch(x))
-      x = self.output(x)
+    def load_weights_from_keras(self, weights):
+        assert self.use_viewdirs, "Not implemented if use_viewdirs=False"
+        
+        # Load pts_linears
+        for i in range(self.D):
+            idx_pts_linears = 2 * i
+            self.pts_linears[i].weight.data = torch.from_numpy(np.transpose(weights[idx_pts_linears]))    
+            self.pts_linears[i].bias.data = torch.from_numpy(np.transpose(weights[idx_pts_linears+1]))
+        
+        # Load feature_linear
+        idx_feature_linear = 2 * self.D
+        self.feature_linear.weight.data = torch.from_numpy(np.transpose(weights[idx_feature_linear]))
+        self.feature_linear.bias.data = torch.from_numpy(np.transpose(weights[idx_feature_linear+1]))
 
-      # Concatenate alphas to output
-      x = torch.concat([x, alpha], dim=-1)
-    else:
-      # Simple output
-      x = self.output(x)
-    return x
+        # Load views_linears
+        idx_views_linears = 2 * self.D + 2
+        self.views_linears[0].weight.data = torch.from_numpy(np.transpose(weights[idx_views_linears]))
+        self.views_linears[0].bias.data = torch.from_numpy(np.transpose(weights[idx_views_linears+1]))
+
+        # Load rgb_linear
+        idx_rbg_linear = 2 * self.D + 4
+        self.rgb_linear.weight.data = torch.from_numpy(np.transpose(weights[idx_rbg_linear]))
+        self.rgb_linear.bias.data = torch.from_numpy(np.transpose(weights[idx_rbg_linear+1]))
+
+        # Load alpha_linear
+        idx_alpha_linear = 2 * self.D + 6
+        self.alpha_linear.weight.data = torch.from_numpy(np.transpose(weights[idx_alpha_linear]))
+        self.alpha_linear.bias.data = torch.from_numpy(np.transpose(weights[idx_alpha_linear+1]))
+
 
 
 # Ray helpers
-
-def get_rays(H, W, focal, c2w):
-    """Get ray origins, directions from a pinhole camera."""
-    i, j = torch.meshgrid(torch.range(0, W, dtype=torch.float32),
-                       torch.range(0, H, dtype=torch.float32), indexing='xy')
-    dirs = torch.stack([(i-W*.5)/focal, -(j-H*.5)/focal, -torch.ones_like(i)], -1)
-    rays_d = torch.sum(dirs[..., np.newaxis, :] * c2w[:3, :3], -1)
-    rays_o = torch.broadcast_to(c2w[:3, -1], rays_d.shape)
+def get_rays(H, W, K, c2w):
+    i, j = torch.meshgrid(torch.linspace(0, W-1, W), torch.linspace(0, H-1, H))  # pytorch's meshgrid has indexing='ij'
+    i = i.t()
+    j = j.t()
+    dirs = torch.stack([(i-K[0][2])/K[0][0], -(j-K[1][2])/K[1][1], -torch.ones_like(i)], -1)
+    # Rotate ray directions from camera frame to the world frame
+    rays_d = torch.sum(dirs[..., np.newaxis, :] * c2w[:3,:3], -1)  # dot product, equals to: [c2w.dot(dir) for dir in dirs]
+    # Translate camera frame's origin to the world frame. It is the origin of all rays.
+    rays_o = c2w[:3,-1].expand(rays_d.shape)
     return rays_o, rays_d
 
 
-def get_rays_np(H, W, focal, c2w):
-    """Get ray origins, directions from a pinhole camera."""
-    i, j = np.meshgrid(np.arange(W, dtype=np.float32),
-                       np.arange(H, dtype=np.float32), indexing='xy')
-    dirs = np.stack([(i-W*.5)/focal, -(j-H*.5)/focal, -np.ones_like(i)], -1)
-    rays_d = np.sum(dirs[..., np.newaxis, :] * c2w[:3, :3], -1)
-    rays_o = np.broadcast_to(c2w[:3, -1], np.shape(rays_d))
+def get_rays_np(H, W, K, c2w):
+    i, j = np.meshgrid(np.arange(W, dtype=np.float32), np.arange(H, dtype=np.float32), indexing='xy')
+    dirs = np.stack([(i-K[0][2])/K[0][0], -(j-K[1][2])/K[1][1], -np.ones_like(i)], -1)
+    # Rotate ray directions from camera frame to the world frame
+    rays_d = np.sum(dirs[..., np.newaxis, :] * c2w[:3,:3], -1)  # dot product, equals to: [c2w.dot(dir) for dir in dirs]
+    # Translate camera frame's origin to the world frame. It is the origin of all rays.
+    rays_o = np.broadcast_to(c2w[:3,-1], np.shape(rays_d))
     return rays_o, rays_d
 
 
 def ndc_rays(H, W, focal, near, rays_o, rays_d):
-    """Normalized device coordinate rays.
-
-    Space such that the canvas is a cube with sides [-1, 1] in each axis.
-
-    Args:
-      H: int. Height in pixels.
-      W: int. Width in pixels.
-      focal: float. Focal length of pinhole camera.
-      near: float or array of shape[batch_size]. Near depth bound for the scene.
-      rays_o: array of shape [batch_size, 3]. Camera origin.
-      rays_d: array of shape [batch_size, 3]. Ray direction.
-
-    Returns:
-      rays_o: array of shape [batch_size, 3]. Camera origin in NDC.
-      rays_d: array of shape [batch_size, 3]. Ray direction in NDC.
-    """
     # Shift ray origins to near plane
-    t = -(near + rays_o[..., 2]) / rays_d[..., 2]
-    rays_o = rays_o + t[..., None] * rays_d
-
+    t = -(near + rays_o[...,2]) / rays_d[...,2]
+    rays_o = rays_o + t[...,None] * rays_d
+    
     # Projection
-    o0 = -1./(W/(2.*focal)) * rays_o[..., 0] / rays_o[..., 2]
-    o1 = -1./(H/(2.*focal)) * rays_o[..., 1] / rays_o[..., 2]
-    o2 = 1. + 2. * near / rays_o[..., 2]
+    o0 = -1./(W/(2.*focal)) * rays_o[...,0] / rays_o[...,2]
+    o1 = -1./(H/(2.*focal)) * rays_o[...,1] / rays_o[...,2]
+    o2 = 1. + 2. * near / rays_o[...,2]
 
-    d0 = -1./(W/(2.*focal)) * \
-        (rays_d[..., 0]/rays_d[..., 2] - rays_o[..., 0]/rays_o[..., 2])
-    d1 = -1./(H/(2.*focal)) * \
-        (rays_d[..., 1]/rays_d[..., 2] - rays_o[..., 1]/rays_o[..., 2])
-    d2 = -2. * near / rays_o[..., 2]
-
-    rays_o = torch.stack([o0, o1, o2], -1)
-    rays_d = torch.stack([d0, d1, d2], -1)
-
+    d0 = -1./(W/(2.*focal)) * (rays_d[...,0]/rays_d[...,2] - rays_o[...,0]/rays_o[...,2])
+    d1 = -1./(H/(2.*focal)) * (rays_d[...,1]/rays_d[...,2] - rays_o[...,1]/rays_o[...,2])
+    d2 = -2. * near / rays_o[...,2]
+    
+    rays_o = torch.stack([o0,o1,o2], -1)
+    rays_d = torch.stack([d0,d1,d2], -1)
+    
     return rays_o, rays_d
 
 
-# Hierarchical sampling helper
-
-def sample_pdf(bins, weights, N_samples, det=False):
-
+# Hierarchical sampling (section 5.2)
+def sample_pdf(bins, weights, N_samples, det=False, pytest=False):
     # Get pdf
-    weights += 1e-5  # prevent nans
-    pdf = weights / torch.sum(weights, -1, keepdims=True)
+    weights = weights + 1e-5 # prevent nans
+    pdf = weights / torch.sum(weights, -1, keepdim=True)
     cdf = torch.cumsum(pdf, -1)
-    cdf = torch.cat([torch.zeros_like(cdf[..., :1]), cdf], -1)
+    cdf = torch.cat([torch.zeros_like(cdf[...,:1]), cdf], -1)  # (batch, len(bins))
 
     # Take uniform samples
     if det:
-        u = torch.linspace(0., 1., N_samples)
-        u = torch.broadcast_to(u, list(cdf.shape[:-1]) + [N_samples])
+        u = torch.linspace(0., 1., steps=N_samples)
+        u = u.expand(list(cdf.shape[:-1]) + [N_samples])
     else:
         u = torch.rand(list(cdf.shape[:-1]) + [N_samples])
 
-    # Invert CDF
-    inds = torch.searchsorted(cdf, u, side='right')
-    below = torch.maximum(0, inds-1)
-    above = torch.minimum(cdf.shape[-1]-1, inds)
-    inds_g = torch.stack([below, above], -1)
-    cdf_g = torch.gather(cdf, inds_g, axis=-1, batch_dims=len(inds_g.shape)-2)
-    bins_g = torch.gather(bins, inds_g, axis=-1, batch_dims=len(inds_g.shape)-2)
+    # Pytest, overwrite u with numpy's fixed random numbers
+    if pytest:
+        np.random.seed(0)
+        new_shape = list(cdf.shape[:-1]) + [N_samples]
+        if det:
+            u = np.linspace(0., 1., N_samples)
+            u = np.broadcast_to(u, new_shape)
+        else:
+            u = np.random.rand(*new_shape)
+        u = torch.Tensor(u)
 
-    denom = (cdf_g[..., 1]-cdf_g[..., 0])
-    denom = torch.where(denom < 1e-5, torch.ones_like(denom), denom)
-    t = (u-cdf_g[..., 0])/denom
-    samples = bins_g[..., 0] + t * (bins_g[..., 1]-bins_g[..., 0])
+    # Invert CDF
+    u = u.contiguous()
+    inds = torch.searchsorted(cdf, u, right=True)
+    below = torch.max(torch.zeros_like(inds-1), inds-1)
+    above = torch.min((cdf.shape[-1]-1) * torch.ones_like(inds), inds)
+    inds_g = torch.stack([below, above], -1)  # (batch, N_samples, 2)
+
+    # cdf_g = tf.gather(cdf, inds_g, axis=-1, batch_dims=len(inds_g.shape)-2)
+    # bins_g = tf.gather(bins, inds_g, axis=-1, batch_dims=len(inds_g.shape)-2)
+    matched_shape = [inds_g.shape[0], inds_g.shape[1], cdf.shape[-1]]
+    cdf_g = torch.gather(cdf.unsqueeze(1).expand(matched_shape), 2, inds_g)
+    bins_g = torch.gather(bins.unsqueeze(1).expand(matched_shape), 2, inds_g)
+
+    denom = (cdf_g[...,1]-cdf_g[...,0])
+    denom = torch.where(denom<1e-5, torch.ones_like(denom), denom)
+    t = (u-cdf_g[...,0])/denom
+    samples = bins_g[...,0] + t * (bins_g[...,1]-bins_g[...,0])
 
     return samples
